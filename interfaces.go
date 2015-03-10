2f1c575529e0ef9c6a00d879209a3c98ad17dd9d
Andrey K <andreykvital@gmail.com>
2015-03-10 08:33:18 -0300

    Fixing typo (s/`provices`/`provide`)!

--- interfaces.go
+++ interfaces.go
@@ -1,6 +1,6 @@
 package runescanner
 
-// RuneScanner scans runes in a `io.RuneScanner` and also provices the
+// RuneScanner scans runes in a `io.RuneScanner` and also provides the
 // `LookAhead` method which allows you to get the `n-th` char ahead of the
 // current position
 type RuneScanner interface {
